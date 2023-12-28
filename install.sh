0dbf3403f77b93ad495d7b330e9af512bb715c6a
risto <risto.kowaczewski@gmail.com>
2023-12-28 19:13:14 +0100

    elastic

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 # Namespaces
 
+kubectl create namespace argocd
 kubectl create namespace starrocks
 kubectl create namespace pulsar
 kubectl create namespace elastic
@@ -9,7 +10,10 @@ kubectl create namespace redis
 # Install all required operators, etc.
 
 ## Argo
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+### Setup argo
+kubectl apply -f setup/argocd-setup.yaml
 
 ## Pulsar
 
@@ -22,10 +26,29 @@ helm repo add starrocks-community https://starrocks.github.io/starrocks-kubernet
 
 ## Elastic
 
-kubectl apply -f https://download.elastic.co/downloads/eck/2.1.0/crds.yaml
-kubectl apply -f https://download.elastic.co/downloads/eck/2.1.0/operator.yaml
+# Operators in helm
+
+#kubectl apply -f https://download.elastic.co/downloads/eck/2.1.0/crds.yaml
+#kubectl apply -f https://download.elastic.co/downloads/eck/2.1.0/operator.yaml
 
 
 ## Percona
 
 helm repo add percona https://percona.github.io/percona-helm-charts/
+
+
+# Installation
+
+## Cert manager
+
+cd /tmp
+git clone https://github.com/apache/pulsar-helm-chart
+cd pulsar-helm-chart
+./scripts/cert-manager/install-cert-manager.sh
+
+
+## Prepare pulsar helm release
+
+git clone https://github.com/apache/pulsar-helm-chart
+cd pulsar-helm-chart
+./scripts/pulsar/prepare_helm_release.sh -n pulsar -k pulsar
