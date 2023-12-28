ee25914f26974c64f814a3557a453489f99ae4f8
risto <risto.kowaczewski@gmail.com>
2023-12-28 20:38:41 +0100

    elastic

--- install.sh
+++ install.sh
@@ -32,8 +32,8 @@ helm repo add starrocks-community https://starrocks.github.io/starrocks-kubernet
 
 # Operators in helm
 
-#kubectl apply -f https://download.elastic.co/downloads/eck/2.1.0/crds.yaml
-#kubectl apply -f https://download.elastic.co/downloads/eck/2.1.0/operator.yaml
+kubectl apply -f https://download.elastic.co/downloads/eck/2.1.0/crds.yaml
+kubectl apply -f https://download.elastic.co/downloads/eck/2.1.0/operator.yaml
 
 
 ## Percona
