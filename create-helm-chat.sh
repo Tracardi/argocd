ac7a9a071b011fdb89dd26ca86d795db914c303a
risto <risto.kowaczewski@gmail.com>
2024-09-23 23:36:19 +0200

    chore: update Helm chart to version 1.0.2
    Create directory for version 1.0.2 in tracardi-helm-repo.
    Update Helm package and move it to version 1.0.2 directory.
    Update helm repo index and comment scp command for version 1.0.2.
    Update Chart.yaml with version 1.0.2.

--- create-helm-chat.sh
+++ create-helm-chat.sh
@@ -1,4 +1,5 @@
+mkdir -p tracardi-helm-repo/1.0.2
 helm package tracardi
-mv tracardi-1.0.0.tgz tracardi-helm-repo/1.0.0
-helm repo index tracardi-helm-repo/1.0.0 --url https://www.tracardi.com/helm/1.0.0/
-#scp -P 222 -r tracardi-helm-repo/1.0.0 kuptoo@s113.linuxpl.com:/domains/tracardi.com/public_html/helm/1.0.0
+mv tracardi-1.0.2.tgz tracardi-helm-repo/1.0.2
+helm repo index tracardi-helm-repo/1.0.2 --url https://www.tracardi.com/helm/1.0.2/
+#scp -P 222 -r tracardi-helm-repo/1.0.2 kuptoo@s113.linuxpl.com:/domains/tracardi.com/public_html/helm/1.0.2
