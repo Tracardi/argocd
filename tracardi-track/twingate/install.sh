f799d52b7605b7b49181307d771603f62540ef18
risto <risto.kowaczewski@gmail.com>
2024-10-01 08:28:03 +0200

    chore: update image tags to version 1.1.0 in values.yaml

--- tracardi-track/twingate/install.sh
+++
@@ -1,2 +0,0 @@
-kubectl apply -f twingate-namespace.yaml
-kubectl apply -f twingate-nginx-forwarder.yaml -n twingate-forwarder
--- tracardi-track/twingate/remove.sh
+++
@@ -1,2 +0,0 @@
-kubectl delete -f twingate-nginx-forwarder.yaml -n twingate-forwarder
-kubectl delete -f twingate-namespace.yaml
