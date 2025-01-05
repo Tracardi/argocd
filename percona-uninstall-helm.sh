b9e23baead0b96aaddc5104a2d6ae959ad69707f
risto <risto.kowaczewski@gmail.com>
2025-01-05 17:58:16 +0100

    Fix order of helm delete commands and comment namespace deletion.
    Reordered helm delete commands to ensure proper resource cleanup. Commented out the namespace deletion to prevent unintended removal of the namespace during uninstallation.

--- percona-uninstall-helm.sh
+++ percona-uninstall-helm.sh
@@ -2,7 +2,7 @@ NS="percona"
 
 kubectl delete pxc percona-db-pxc-db -n $NS
 
-helm delete percona-db --namespace $NS
 helm delete percona-op --namespace $NS
+helm delete percona-db --namespace $NS
 
-kubectl delete namespace $NS
+#kubectl delete namespace $NS
