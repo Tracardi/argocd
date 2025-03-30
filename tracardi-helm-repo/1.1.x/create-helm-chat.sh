00e14778fed3b9d3e1d9f234526ad9a62a29e9c0
risto <risto.kowaczewski@gmail.com>
2025-03-30 14:05:27 +0200

    new version

--- tracardi-helm-repo/1.1.x/create-helm-chat.sh
+++ tracardi-helm-repo/1.1.x/create-helm-chat.sh
@@ -1,3 +1,4 @@
 helm package ../../tracardi
-helm repo index . --url https://www.tracardi.com/helm/1.1.x/
+helm repo index . --url https://www.tracardi.com/helm/2.0.x/
+#helm repo index . --url https://www.tracardi.com/helm/1.1.x/
 #scp -P 222 -r tracardi-helm-repo/1.0.2 kuptoo@s113.linuxpl.com:/domains/tracardi.com/public_html/helm/1.0.2
