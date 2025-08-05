4aebea2fb2f6aabb8c052299be61f9990ae760ca
risto <risto.kowaczewski@gmail.com>
2025-08-05 13:04:24 +0200

    ``` feat(pulsar, elastic): add production values files and update configs (1.2.x) - Disable backup in percona local values - Update storage request size to 1Gi in percona local values - Add cloud provider configuration for Hetzner in tracardi values - Include annotations for load balancer in multiple tracardi services - Change helm install to upgrade in percona-install-helm.sh - Add new script for installing tracardi with specific values ```

---
+++ 11x-tracardi-install-helm.sh
@@ -0,0 +1,13 @@
+NS="tracardi-11x"
+VALUES="110-verismart-com-values.yaml"
+DOCKERHUB=""
+
+kubectl create ns $NS
+
+kubectl create secret docker-registry tracardi-dockerhub \
+    --docker-server=index.docker.io/v1/  \
+    --docker-username=tracardi \
+    --docker-password=$DOCKERHUB \
+    -n $NS
+
+helm upgrade --install tracardi tracardi -f values/$VALUES -n $NS
--- percona-install-helm.sh
+++ percona-install-helm.sh
@@ -1,5 +1,5 @@
 NS="percona"
 
 kubectl create namespace $NS
-helm install percona-op percona/pxc-operator --namespace $NS
-helm install percona-db percona/pxc-db --values percona/local-values.yaml --namespace $NS
+helm upgrade --install percona-op percona/pxc-operator --namespace $NS
+helm upgrade --install percona-db percona/pxc-db --values percona/local-values.yaml --namespace $NS
